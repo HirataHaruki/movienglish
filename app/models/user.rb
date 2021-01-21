f5a8232e82ef2a5b0537e19bbb66a343812ee294
Haruki Hirata <haru922611@gmail.com>
2021-01-21 14:39:33 +0000

    1月21日

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  EMAIL_VALIDATION = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: EMAIL_VALIDATION }
   validates :password, presence: true, length: { minimum: 8 }
   
   has_secure_password
